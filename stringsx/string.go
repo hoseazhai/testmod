51754a4bdeb964694627e8bea4b86a3ea4d12f84
Administrator <admin@example.com>
2019-11-03 15:16:10 +0800

    ddddtest

--- stringsx/string.go
+++ stringsx/string.go
@@ -3,5 +3,5 @@ package stringsx
 import "fmt"
 
 func Hello(name string) string{
-	return fmt.Sprintf("Hello tttttttvvvvddddddddddddddddd4444, %s!!!", name)
+	return fmt.Sprintf("Hello test zzzzzzzzz, %s!!!", name)
 }
